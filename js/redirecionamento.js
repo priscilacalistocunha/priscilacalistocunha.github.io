5ea129478866c6d5a7c5e0ff4239745e2dec4554
priscilacalistocunha <151700817+priscilacalistocunha@users.noreply.github.com>
2023-11-22 10:17:26 +0000

    #:Ajuste js

--- js/redirecionamento.js
+++ js/redirecionamento.js
@@ -1,9 +1,16 @@
 function redirecionarUrl(urlDispositivos, urlPadrao) {
     var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-    if (/android|iPad|iPhone/.test(userAgent)) {
-        return window.location.replace(urlDispositivos);
+    // Verifica se é um dispositivo Android
+    if (/android/i.test(userAgent)) {
+        window.location.replace(urlDispositivos);
     }
-
-    return window.location.replace(urlPadrao);
+    // Verifica se é um dispositivo iOS
+    else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        window.location.replace(urlDispositivos);
+    }
+    // Se não for Android nem iOS, redireciona para o URL padrão
+    else {
+        window.location.replace(urlPadrao);
+    }
 }
